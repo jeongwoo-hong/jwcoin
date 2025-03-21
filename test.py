d8ec37aec57000e986510c601f06c59b4a783947
jingwoo <ghdepddn@snu.ac.kr>
2025-03-21 14:22:32 +0900

    25.3.21.1 test adding image date

--- test.py
+++ test.py
@@ -3,5 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-print("Hello Coin World")
-print(os.getenv("UPBIT_ACCESS_KEY"))
+# print("Hello Coin World")
+# print(os.getenv("UPBIT_ACCESS_KEY"))
+
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('http://selenium.dev/')
